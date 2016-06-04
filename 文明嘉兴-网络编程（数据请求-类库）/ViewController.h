a0849106a0b6e8ff5769811f36f18ff2812419d2
DC004 <dc004@duandonggang.lan>
2016-06-04 10:08:51 +0800

    Initial Commit

---
+++ 文明嘉兴-网络编程（数据请求-类库）/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  文明嘉兴-网络编程（数据请求-类库）
+//
+//  Created by DC004 on 16/6/4.
+//  Copyright © 2016年 段东刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 文明嘉兴-网络编程（数据请求-类库）/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  文明嘉兴-网络编程（数据请求-类库）
+//
+//  Created by DC004 on 16/6/4.
+//  Copyright © 2016年 段东刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
